51e3ea8eb1f762c3797a1a940301744e62d265d3
KristjanPH <ph.kristjan@gmail.com>
2025-08-30 15:22:24 +0200

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,21 @@
+
+prva sekcija kdo smo/kaj nudimo (hero page)
+
+logo/font za ime (levo v header)
+
+ostale sekcije kot storitve (parallax drevo)
+
+
+sections
+    0 home (klik na logo, kot active nastavi section 1)
+    1 kdo smo
+    2 obrezovanje
+    3 .
+    4 .
+    5 .
+    6 .
+    7 about (footer)
+
+kdo smo preberi več -> rubnov text posebi stran
+footer styles
+mobile
---
+++ script.js
@@ -0,0 +1,45 @@
+function scrollToSection(id) {
+    // remove 'active' class from previous button
+    const activeNavButton = document.getElementsByClassName('active');
+    if (activeNavButton.length > 0) {
+        activeNavButton[0].classList.remove('active');
+    }
+    
+    // add 'active' class to clicked section button in navigation bar
+    document.getElementById(`button${id}`).classList.add('active');
+
+    // enable smooth scrolling between sections
+    switch (id) {
+        case 0:
+            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+            break;
+        
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            document.getElementById(`section${id}`).scrollIntoView({ behavior: 'smooth' });
+            break;
+        
+        case 6:
+            document.getElementById(`footer`).scrollIntoView({ behavior: 'smooth' });
+            break;
+    }
+}
+
+window.onload = () => {
+    document.querySelector('.parallax-image').style.height = `${document.body.offsetHeight - window.innerHeight}px`;
+    document.querySelector('.parallax-image').style.top = `${-1.2*window.innerHeight}px`;
+}
+
+// background parallax effect
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset;
+    document.querySelector('.parallax-image').style.transform = `translateY(${scrollTop * -0.3}px)`;
+});
+
+// on page refresh scroll to top
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+}
