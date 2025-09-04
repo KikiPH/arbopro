2619a9449e78bbc91a270131815aaf772440f4a3
KikiPH <ph.kristjan@gmail.com>
2025-09-04 10:26:11 +0200

    Tab icon, mail form, scrolling update

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 
 TODO:
 - hero page - sekcija kot kratek opis vseh storitev ?
-- logo (header levo)
 - galerija (slike iz terena)
-- mail povpraševanje (forma na dnu pred/zraven kontakta)
-- preberi več (redirect na nov subpage za vsako storitev)
 - mobile
-- stili
+
+- preberi več (redirect na nov subpage za vsako storitev)
+
+- scroll down pojavi scroll to top button
--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+
+// set parallax image height and position
+window.onload = () => {
+    document.querySelector('.parallax-image').style.height = `${document.body.offsetHeight - window.innerHeight}px`;
+    document.querySelector('.parallax-image').style.top = `${-1.2*window.innerHeight}px`;
+}
+
+// background parallax effect
+window.addEventListener('scroll', () => {
+
+    // scroll parallax image
+    let scrollTop = window.pageYOffset;
+    document.querySelector('.parallax-image').style.transform = `translateY(${scrollTop * -0.3}px)`;
+
+    // dinamically update active button in navigation bar
+    let sections = document.getElementsByTagName('section');
+    for (let i = 1; i <= sections.length + 1; i++) { // +1 for footer section
+
+        let sectionButton = document.getElementById(`button${i}`);
+        let sectionContent = document.getElementById(`section${i}-content`);
+
+        if (isFullyVisible(sectionContent)) {
+            sectionButton.classList.add('active');
+        } else {
+            // edge case:
+            // while scrolling and between sections
+            // only remove active class from previous section if next section is fully visible
+            let activeSections = document.getElementsByClassName('active');
+            if (activeSections.length > 1) sectionButton.classList.remove('active');
+        }
+    }
+    
+    // edge case:
+    // if both the last section and the footer section are fully visible
+    // only highlight the footer section button
+    let lastSection = document.getElementById(`section${sections.length}-content`);
+    let footerSection = document.getElementById(`section${sections.length + 1}-content`);
+    console.log(lastSection, footerSection);
+    if (isFullyVisible(lastSection) && isFullyVisible(footerSection)) {
+        document.getElementById(`button${sections.length}`).classList.remove('active');
+    }
+});
+
+// on page refresh scroll to top
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+}
+
+// ------------------------------------------------
+// helper functions
+// ------------------------------------------------
+
+// returns true if the entire HTML element is visible in the viewport
+function isFullyVisible(el) {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// scrolls to section and updates active button in navigation bar
 function scrollToSection(id) {
     // remove 'active' class from previous button
     const activeNavButton = document.getElementsByClassName('active');
@@ -9,38 +74,10 @@ function scrollToSection(id) {
     document.getElementById(`button${id}`)?.classList.add('active');
 
     // enable smooth scrolling between sections
-    switch (id) {
-        case 0:
-            document.getElementById('button1').classList.add('active'); // set first section as active
-            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-            break;
-        
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            document.getElementById(`section${id}`).scrollIntoView({ behavior: 'smooth' });
-            break;
-        
-        case 6:
-            document.getElementById(`footer`).scrollIntoView({ behavior: 'smooth' });
-            break;
-    }
+    document.getElementById(`section${id}`).scrollIntoView({ behavior: 'smooth' });
 }
 
-window.onload = () => {
-    document.querySelector('.parallax-image').style.height = `${document.body.offsetHeight - window.innerHeight}px`;
-    document.querySelector('.parallax-image').style.top = `${-1.2*window.innerHeight}px`;
-}
-
-// background parallax effect
-window.addEventListener('scroll', () => {
-    let scrollTop = window.pageYOffset;
-    document.querySelector('.parallax-image').style.transform = `translateY(${scrollTop * -0.3}px)`;
-});
-
-// on page refresh scroll to top
-window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
+// redirects to homepage
+function goToHomepage() {
+    window.location = '/';
 }
